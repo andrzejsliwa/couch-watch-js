836880805695f9ea06a8e3ddd48247415a26599e
andrzejsliwa <andrzej.sliwa@i-tool.eu>
2011-03-04 14:59:14 +0100

    cleanup

--- src/couchwatch.js
+++ src/couchwatch.js
@@ -3,7 +3,7 @@ $(function() {
   window.couchwatch.enableCouchWatch = function (db_url) {
     function emptyFunction(){}
     _.each(["log", "debug", "info", "warn", "error"], function (method) {
-      couchwatch[method] || (couchwatch[method] = emptyFunction);;
+      couchwatch[method] || (couchwatch[method] = emptyFunction);
       couchwatch[method] = (function() {
         return function(message) {
           $.post(db_url + "/_design/couchwatch/_update/logger", {severity: method, message: message});
